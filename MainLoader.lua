24ade1c9e153bed4a090e44fb73687944f35b396
Serial-Zero <m0rt1f1edfoxfire@gmail.com>
2026-01-24 23:30:32 -0600

    Enhance MainLoader.lua to conditionally load Universal.lua based on game ID; update Universal.lua to integrate WindUI for improved UI management.

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadSignalFound/SOLVER/refs/heads/main/Universal.lua"))()
+gameId = game.PlaceId
+if gameId == 3260590327 then
+    print("test")
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadSignalFound/SOLVER/refs/heads/main/Universal.lua"))()
+end
--- scripts/Universal.lua
+++ scripts/Universal.lua
@@ -1,1 +1,30 @@
-print("Universal Loaded!")
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+local Window = WindUI:CreateWindow({
+    Title = "My Super Hub",
+    Icon = "door-open", -- lucide icon
+    Author = "by .ftgs and .ftgs",
+    Folder = "MySuperHub",
+    
+
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    
+
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+            print("clicked")
+        end,
+    },
+    
+})
