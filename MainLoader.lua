f35929d64ac089b857760002c6f3946eb7e6e2a7
Serial-Zero <m0rt1f1edfoxfire@gmail.com>
2026-01-24 23:32:12 -0600

    Update MainLoader.lua to change the URL for loading Universal.lua, ensuring it points to the correct script location.

--- MainLoader.lua
+++ MainLoader.lua
@@ -2,5 +2,5 @@ gameId = game.PlaceId
 if gameId == 3260590327 then
     print("test")
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadSignalFound/SOLVER/refs/heads/main/Universal.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DeadSignalFound/SOLVER/refs/heads/main/scripts/Universal.lua"))()
 end
